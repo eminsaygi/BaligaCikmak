4fa7efe607b1a88d0b255d498a9c9986f098c224
Emin Saygı <emin_0001@hotmail.com>
2023-05-07 19:22:30 +0300

    Storyboard objects added

--- KokAlmaOyunu/ViewController.swift
+++ KokAlmaOyunu/ViewController.swift
@@ -1,19 +1,15 @@
-//
-//  ViewController.swift
-//  KokAlmaOyunu
-//
-//  Created by Emin Saygı on 7.05.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+    
+    @IBOutlet weak var box1: UILabel!
+    @IBOutlet weak var box2: UILabel!
+    @IBOutlet weak var box3: UILabel!
+    @IBOutlet weak var box4: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    
 
 }
-
